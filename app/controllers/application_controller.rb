3602843671e58708072edf1d80fcb1a1758b043b
github username <github email address>
2020-12-11 00:23:57 -0600

    added HTTParty gem and hit zomato api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
+require 'rubygems'
+require 'httparty'
+
 class ApplicationController < ActionController::API
-    
+    include HTTParty
+
     def city_cuisines
-        puts "hit /city_cuisines endpoint"
+        response = HTTParty.get("https://developers.zomato.com/api/v2.1/cities?q=#{params[:city_name]}", {headers: {"user-key": params[:api_key]}})
+        
     end
 
     def cuisine_daily_menus
         puts "hit /cuisine_daily_menus endpoint"
     end 
+
+    private
+    def zomato_params
+        params.permit(:city_name, :api_key)
+    end
 end
