7b0d92a330a4ab49e3b4769bd7b49bc7e2356004
github username <github email address>
2020-12-14 01:30:06 -0600

    completed cuisine_daily_menus

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,36 @@ class ApplicationController < ActionController::API
     end
 
     def cuisine_daily_menus
-        puts "hit /cuisine_daily_menus endpoint"
+        response = HTTParty.get("#{@@base_url}/search?entity_id=#{params[:city_id]}&entity_type=city&count=2&cuisines=#{params[:cuisine_id]}",  {headers: {"user-key": params[:api_key]}})
+        if response['restaurants'].length == 0
+            render json: {error: 'no restaurants found'}
+        else 
+           menus = aggregate_menus(response['restaurants'])
+           if menus.length == 0
+            render json: {"error": "no daily menus found"}
+           else 
+            if menus.length > 3 
+                menus = menus.slice(0,3)
+            end 
+            render json: {menus: menus}
+           end
+        end 
+        
     end 
 
     private
     def zomato_params
-        params.permit(:city_name, :api_key)
+        params.permit(:city_name, :api_key, :city_id, :cuisine_id)
     end
+
+    def aggregate_menus(restaurants)
+        daily_menus = []
+        for restaurant in restaurants do
+            daily_menu_response = HTTParty.get("#{@@base_url}/dailymenu?res_id=#{restaurant["restaurant"]['id']}", {headers: {"user-key": params[:api_key]}})
+            if daily_menu_response.code == 200
+                daily_menus << daily_menu_response
+            end 
+        end
+        return daily_menus
+    end 
 end
